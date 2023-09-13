97a2811c470cb061b02ce596e8db505994f35a63
Alwafauzan <alwafauzan123@gmail.com>
2023-09-13 10:14:55 +0700

    a

---
+++ arduino/pulse_sensor_basic.ino
@@ -0,0 +1,19 @@
+int PulseSensorPurplePin1 = 4;
+int PulseSensorPurplePin2 = 14;
+int Signal1;
+int Signal2;
+//int Threshold = 580;
+void setup(){
+  Serial.begin(115200);
+};
+void loop(){
+  Signal1 = analogRead(PulseSensorPurplePin1);
+  Signal2 = analogRead(PulseSensorPurplePin2);  
+  Serial.print(Signal1); 
+  Serial.print(", ");
+  Serial.print(Signal2); 
+  Serial.print(", ");
+  Serial.print(int(millis()/1000)%3000);
+  Serial.println();
+delay(10);
+};
---
+++ arduino/pulse_sensor_bocil.ino
@@ -0,0 +1,14 @@
+//int PulseSensorPurplePin = 4;
+//int Signal;
+////int Threshold = 580;
+//void setup(){
+//  Serial.begin(9600);
+//}
+//void loop(){
+//  Signal = analogRead(PulseSensorPurplePin);  
+//  Serial.print(Signal); 
+//  Serial.print(", ");
+//  Serial.print(int(millis()/1000)%3000);
+//  Serial.println();
+//delay(10);
+//}
